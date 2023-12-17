2e63224963adf52c0be0b26a220ded87a8d0a8ce
Geoyeob Kim <gasbank@gmail.com>
2023-12-17 18:37:42 +0900

    각 Segment Group의 코너 삼각형 RGB 컬러로 표기

--- Assets/Sphere.cs
+++ Assets/Sphere.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Sphere : MonoBehaviour {
     public Material mat;
+    public Material[] matRgb;
     public MeshFilter meshFilter;
 
     // 황금비를 이루는 직사각형의 너비와 높이 계산
@@ -13,52 +14,130 @@ public class Sphere : MonoBehaviour {
     // Wh: 직사각형 너비의 절반
     static readonly float Hh = 2 / Mathf.Sqrt(10 + 2 * Mathf.Sqrt(5));
     static readonly float Wh = Hh * (1 + Mathf.Sqrt(5)) / 2;
-    
+
     readonly int[][] edgesPerFaces = {
-        new[]{0, 1, 7},
-        new[]{0, 4, 1},
-        new[]{0, 7, 9},
-        new[]{0, 8, 4},
-        new[]{0, 9, 8},
-        new[]{1,11, 10},
-        new[]{1,10, 7},
-        new[]{1,4, 11},
-        new[]{2,3, 6},
-        new[]{2,5, 3},
-        new[]{2,6, 10},
-        new[]{2,10, 11},
-        new[]{2,11, 5},
-        new[]{3,5, 8},
-        new[]{3,8, 9},
-        new[]{3,9, 6},
-        new[]{4,5, 11},
-        new[]{4,8, 5},
-        new[]{6,7, 10},
-        new[]{6,9, 7},
+        new[] {
+            0,
+            1,
+            7,
+        },
+        new[] {
+            0,
+            4,
+            1,
+        },
+        new[] {
+            0,
+            7,
+            9,
+        },
+        new[] {
+            0,
+            8,
+            4,
+        },
+        new[] {
+            0,
+            9,
+            8,
+        },
+        new[] {
+            1,
+            11,
+            10,
+        },
+        new[] {
+            1,
+            10,
+            7,
+        },
+        new[] {
+            1,
+            4,
+            11,
+        },
+        new[] {
+            2,
+            3,
+            6,
+        },
+        new[] {
+            2,
+            5,
+            3,
+        },
+        new[] {
+            2,
+            6,
+            10,
+        },
+        new[] {
+            2,
+            10,
+            11,
+        },
+        new[] {
+            2,
+            11,
+            5,
+        },
+        new[] {
+            3,
+            5,
+            8,
+        },
+        new[] {
+            3,
+            8,
+            9,
+        },
+        new[] {
+            3,
+            9,
+            6,
+        },
+        new[] {
+            4,
+            5,
+            11,
+        },
+        new[] {
+            4,
+            8,
+            5,
+        },
+        new[] {
+            6,
+            7,
+            10,
+        },
+        new[] {
+            6,
+            9,
+            7,
+        },
     };
 
     readonly Vector3[] vertices = {
-        new(0, -Hh,-Wh),
-        new(0, +Hh,-Wh),
-        new(0, +Hh,+Wh),
-        new(0, -Hh,+Wh),
-            
+        new(0, -Hh, -Wh),
+        new(0, +Hh, -Wh),
+        new(0, +Hh, +Wh),
+        new(0, -Hh, +Wh),
         new(-Wh, 0, -Hh),
         new(-Wh, 0, +Hh),
         new(+Wh, 0, +Hh),
         new(+Wh, 0, -Hh),
-            
-        new(-Hh,-Wh,0),
-        new(+Hh,-Wh,0),
-        new(+Hh,+Wh,0),
-        new(-Hh,+Wh,0),
+        new(-Hh, -Wh, 0),
+        new(+Hh, -Wh, 0),
+        new(+Hh, +Wh, 0),
+        new(-Hh, +Wh, 0),
     };
-    
+
     void Start() {
         var mesh = new Mesh { vertices = vertices };
 
         var triangles = edgesPerFaces.SelectMany(e => e).ToArray();
-        
+
         mesh.triangles = triangles;
         mesh.normals = vertices;
         mesh.RecalculateNormals();
@@ -69,20 +148,32 @@ public class Sphere : MonoBehaviour {
 
         meshFilter.mesh = mesh;
 
-        foreach (var edgesPerFace in edgesPerFaces) {
+        const int subdivisionCount = 10;
+
+        for (var index = 0; index < edgesPerFaces.Length; index++) {
+            var edgesPerFace = edgesPerFaces[index];
             var go = new GameObject();
             var mf = go.AddComponent<MeshFilter>();
-            mf.mesh = CreateSubdividedTri(new [] { vertices[edgesPerFace[0]], vertices[edgesPerFace[1]], vertices[edgesPerFace[2]] }, 10);
-            go.AddComponent<MeshRenderer>().material = mat;
+            mf.mesh = CreateSubdividedTri(new[] {
+                vertices[edgesPerFace[0]],
+                vertices[edgesPerFace[1]],
+                vertices[edgesPerFace[2]],
+            }, subdivisionCount);
+            var mr = go.AddComponent<MeshRenderer>();
+            mr.materials = new[] {
+                mat,
+                matRgb[0],
+                matRgb[1],
+                matRgb[2],
+            };
+            go.name = $"Segment Group {index}";
         }
-
-        
     }
-    
-    #if UNITY_EDITOR
-    void OnDrawGizmos() {
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected() {
         Gizmos.color = Color.magenta;
-        
+
         for (var index = 0; index < vertices.Length; index++) {
             var v = vertices[index];
             Gizmos.DrawSphere(v, 0.025f);
@@ -98,7 +189,7 @@ public class Sphere : MonoBehaviour {
         }
     }
 #endif
-    
+
     static Mesh CreateSubdividedTri(IReadOnlyList<Vector3> vList, int n) {
         var totalVCount = (n + 1) * (n + 2) / 2;
         var totalFCount = n * n;
@@ -112,7 +203,7 @@ public class Sphere : MonoBehaviour {
 
         var vertices = new Vector3[totalVCount];
         var edgesPerFaces = new int[totalFCount][];
-        
+
         var vIndex = 0;
         var fIndex = 0;
         for (var b = 0; b <= n; b++) {
@@ -147,12 +238,16 @@ public class Sphere : MonoBehaviour {
         var mesh = new Mesh { vertices = vertices };
 
         var triangles = edgesPerFaces.SelectMany(e => e).ToArray();
-        
+
         mesh.triangles = triangles;
         mesh.normals = vertices;
         mesh.RecalculateNormals();
-
+        mesh.subMeshCount = 4;
+        mesh.SetTriangles(triangles, 0);
+        mesh.SetTriangles(triangles.Take(3).ToList(), 1);
+        mesh.SetTriangles(triangles.Skip(3 * ((n - 1) * 2)).Take(3).ToList(), 2);
+        mesh.SetTriangles(triangles.Skip(3 * (totalFCount - 1)).Take(3).ToList(), 3);
+        
         return mesh;
     }
 }
-
