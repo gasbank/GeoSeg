c13db67bd0fee58ce8d13829ab4af041f9670094
Geoyeob Kim <gasbank@gmail.com>
2024-01-13 20:59:55 +0900

    세그먼트 그룹 삼각형 조금씩 크기 키워서 레이 테스팅 때 삼각형 간 틈으로 테스팅 실패하지 않도록 하기

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -377,6 +377,14 @@ public class Sphere : MonoBehaviour {
 
             segmentGroupTriList.Add(edgeVertices);
         }
+        
+        // 세그먼트 그룹 삼각형이 너무 정확히 맞닿아있으면, 삼각형 연결되는 틈새로
+        // 레이 테스트 시 틈 사이로 지나가서 실패하는 경우가 있다.
+        // 세그먼트 그룹 삼각형을 약간씩 키운다.
+        segmentGroupTriList = segmentGroupTriList.Select(triList => {
+            var center = triList.Aggregate(Vector3.zero, (s, v) => s + v) / 3;
+            return triList.Select(e => e + (e - center).normalized * 1e-3f).ToArray();
+        }).ToList();
 
         Gizmos.color = Color.white;
         Gizmos.DrawLine(Vector3.zero, userPos.position);
